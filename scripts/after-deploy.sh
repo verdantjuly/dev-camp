b819677b36bd2d14cd942784d71621d7d8f52acd
dayounglee <verdantjuly@kakao.com>
2024-03-08 17:02:20 +0900

    feat : 기존 프로젝트 꺼지지 않게 스크립트 추가
    EC2를 같이 써서 한 프로젝트가 배포될 때
    다른 프로젝트가 꺼진다.
    함께 pm2를 시작해 줘서 방진한다.
    
    cd /home/ubuntu/build
    
    sudo npm install
    sudo pm2 kill
    sudo pm2 start "npm run start:gate_build"
    sudo pm2 start "npm run start:dis_build"
    sudo pm2 start "npm run start:users_build"
    sudo pm2 start "npm run start:posts_build"
    sudo pm2 start "npm run start:admin_build"
    sudo pm2 start "npm run start:threads_build"
    sudo pm2 start "npm run start:chat_build"
    
    cd /home/ubuntu/devcamp
    
    sudo yarn install
    sudo pm2 start "yarn run start:prod"
    
    sudo pm2 startup
    sudo pm2 save

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
+cd /home/ubuntu/build
+
+sudo npm install
+sudo pm2 kill
+sudo pm2 start "npm run start:gate_build"
+sudo pm2 start "npm run start:dis_build"
+sudo pm2 start "npm run start:users_build"
+sudo pm2 start "npm run start:posts_build"
+sudo pm2 start "npm run start:admin_build"
+sudo pm2 start "npm run start:threads_build"
+sudo pm2 start "npm run start:chat_build"
+
 cd /home/ubuntu/devcamp
 
 sudo yarn install
 sudo pm2 start "yarn run start:prod"
+
+sudo pm2 startup
+sudo pm2 save
