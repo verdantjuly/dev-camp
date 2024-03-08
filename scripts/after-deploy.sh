1eb459d237c0061ed1a8d6a9b2b817375b81582b
dayounglee <verdantjuly@kakao.com>
2024-03-08 17:43:27 +0900

    after-deploy.sh
    AfterInstall에서의 진행 불가로 인해 원래 상태로 복구

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-cd /home/ubuntu/build
-
-sudo npm install
-sudo pm2 kill
-sudo pm2 start "npm run start:gate_build"
-sudo pm2 start "npm run start:dis_build"
-sudo pm2 start "npm run start:users_build"
-sudo pm2 start "npm run start:posts_build"
-sudo pm2 start "npm run start:admin_build"
-sudo pm2 start "npm run start:threads_build"
-sudo pm2 start "npm run start:chat_build"
-
 cd /home/ubuntu/devcamp
 
 sudo yarn install
